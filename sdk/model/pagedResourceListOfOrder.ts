fd0b0157c5bf4f40926607fd77a9820ce384b2be
Concourse <concourse@finbourne.com>
2020-02-10 15:26:55 +0000

    Update SDK 0.10.1081

--- sdk/model/pagedResourceListOfOrder.ts
+++ sdk/model/pagedResourceListOfOrder.ts
@@ -10,40 +10,47 @@
  * Do not edit the class manually.
  */
 
-import { ValuationReconciliationRequest } from './valuationReconciliationRequest';
+import { Link } from './link';
+import { Order } from './order';
 
-/**
-* Specification for the reconciliation request. Left and Right hand sides are constructed. Each consists of a valuation of a portfolio  using an aggregation request. The results of this can then be compared to each other. The difference, which is effectively a risk based  difference allows comparison of the effects of changing a recipe, valuation date, or (though it may or may not make logical sense) a portfolio.  For instance, one might look at the difference in risk caused by the addition of transaction to a portfolio, or through changing the valuation  methodology or system.
-*/
-export class ValuationsReconciliationRequest {
-    'left': ValuationReconciliationRequest;
-    'right': ValuationReconciliationRequest;
-    /**
-    * Instrument properties to be included with any identified breaks. These properties will be in the effective and AsAt dates of the left portfolio
-    */
-    'instrumentPropertyKeys': Array<string>;
+export class PagedResourceListOfOrder {
+    'nextPage'?: string;
+    'previousPage'?: string;
+    'values'?: Array<Order>;
+    'href'?: string;
+    'links'?: Array<Link>;
 
     static discriminator: string | undefined = undefined;
 
     static attributeTypeMap: Array<{name: string, baseName: string, type: string}> = [
         {
-            "name": "left",
-            "baseName": "left",
-            "type": "ValuationReconciliationRequest"
+            "name": "nextPage",
+            "baseName": "nextPage",
+            "type": "string"
         },
         {
-            "name": "right",
-            "baseName": "right",
-            "type": "ValuationReconciliationRequest"
+            "name": "previousPage",
+            "baseName": "previousPage",
+            "type": "string"
         },
         {
-            "name": "instrumentPropertyKeys",
-            "baseName": "instrumentPropertyKeys",
-            "type": "Array<string>"
+            "name": "values",
+            "baseName": "values",
+            "type": "Array<Order>"
+        },
+        {
+            "name": "href",
+            "baseName": "href",
+            "type": "string"
+        },
+        {
+            "name": "links",
+            "baseName": "links",
+            "type": "Array<Link>"
         }    ];
 
     static getAttributeTypeMap() {
-        return ValuationsReconciliationRequest.attributeTypeMap;
+        return PagedResourceListOfOrder.attributeTypeMap;
     }
 }
 
