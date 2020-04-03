c37c1d880a4e4cdca2c38dedd3cea4f2f99d619f
Victor Popescu <victorpopescu@shaper.local>
2020-04-03 17:34:04 +0100

    added portfolio delete test + holdings test inception

---
+++ sdk/test/tutorials/createHoldings.test.ts
@@ -0,0 +1,8 @@
+// Require the LUSID SDK and libraries
+import {
+  PortfolioHolding, Portfolio } from "../../api";
+
+import { client } from './clientBuilder'
+import { IncomingMessage } from "http";
+import { LusidProblemDetails } from "../../model/models";
+// const uuid4 = require('uuid/v4')
--- sdk/test/tutorials/createPortfolio.test.ts
+++ sdk/test/tutorials/createPortfolio.test.ts
@@ -1,12 +1,13 @@
 // Require the LUSID SDK and libraries
+import expect from 'expect'
+
 import {
   CreateTransactionPortfolioRequest, Portfolio } from "../../api";
 
 import { client } from './clientBuilder'
 import { IncomingMessage } from "http";
-import { LusidProblemDetails } from "../../model/models";
+import { LusidProblemDetails, DeletedEntityResponse } from "../../model/models";
 const uuid4 = require('uuid/v4')
-
 
 function createTransactionPortfolio(
   scope: string,
@@ -17,7 +18,25 @@ function createTransactionPortfolio(
         scope,
         createRequest
       )
-      .then((res: {response: IncomingMessage; body: Portfolio}) => resolve(res.body))
+      .then((res: {response: IncomingMessage; body: Portfolio}) => {
+        resolve( res.body )
+      })
+      .catch((err: {response: IncomingMessage; body: LusidProblemDetails}) => reject(err))
+    })
+  }
+
+function deletePortfolio(
+  scope: string,
+  code: string
+  ) :Promise<DeletedEntityResponse> {
+    return new Promise((resolve, reject) => {
+      client.api.portfolios.deletePortfolio(
+        scope,
+        code
+      )
+      .then((res: {response: IncomingMessage, body: DeletedEntityResponse}) => {
+        resolve( res.body )
+      })
       .catch((err: {response: IncomingMessage; body: LusidProblemDetails}) => reject(err))
     })
   }
@@ -31,9 +50,28 @@ describe('Create portfolios', () => {
   it('Should create a portfolio', (done) => {
     createTransactionPortfolio(uuid4(), createRequest)
     .then((res) => {
-      console.log(res)
+
+      expect( res.id ).toBeInstanceOf( Object )
+
+      this.portfolioData = res;
+
       done()
+
     })
+    .catch((err) => console.log(err.response.statusCode, err.response.statusMessage))
+  })
+
+  it('Should delete the portfolio', (done) => {
+
+    deletePortfolio(
+      this.portfolioData.id.scope,
+      this.portfolioData.id.code
+    ).then( ( res ) => {
+
+      console.log( res );
+
+      done()
+    } )
     .catch((err) => console.log(err.response.statusCode, err.response.statusMessage))
   })
 })
