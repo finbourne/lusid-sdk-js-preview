923bc00eae8df5f3a7f867b6ad4732dea2f24d0b
Victor Popescu <victorpopescu@shaper.local>
2020-04-03 14:47:17 +0100

    removed superflous commentary

--- sdk/test/tutorials/clientBuilder.ts
+++ sdk/test/tutorials/clientBuilder.ts
@@ -1,20 +1,3 @@
 import { Client, Source } from '../../client/client'
 
-/*
-  Read from Environment Variables
-*/
-
-// export var client = new Client(
-//   [Source.Environment, 'FBN_TOKEN_URL'],
-//   [Source.Environment, 'FBN_USERNAME'],
-//   [Source.Environment, 'FBN_PASSWORD'],
-//   [Source.Environment, 'FBN_CLIENT_ID'],
-//   [Source.Environment, 'FBN_CLIENT_SECRET'],
-//   [Source.Environment, 'FBN_LUSID_API_URL'],
-// )
-
-/*
-  Read from secrets.json
-*/
-
 export var client = new Client()
