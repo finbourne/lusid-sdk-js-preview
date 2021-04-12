9aa5c049e386880487fb294555ed49b85a3e4cb9
Concourse <concourse@finbourne.com>
2021-04-12 14:57:28 +0000

    Update SDK 0.11.2840

--- sdk/model/models.ts
+++ sdk/model/models.ts
@@ -13,7 +13,6 @@ export * from './aggregationOp';
 export * from './aggregationOptions';
 export * from './aggregationQuery';
 export * from './aggregationRequest';
-export * from './aggregationResponseNode';
 export * from './aggregationType';
 export * from './allocation';
 export * from './allocationRequest';
@@ -175,7 +174,6 @@ export * from './marketOptions';
 export * from './metricValue';
 export * from './modelSelection';
 export * from './movementType';
-export * from './nestedAggregationResponse';
 export * from './operandType';
 export * from './operator';
 export * from './optionType';
@@ -398,7 +396,6 @@ import { AggregationOp } from './aggregationOp';
 import { AggregationOptions } from './aggregationOptions';
 import { AggregationQuery } from './aggregationQuery';
 import { AggregationRequest } from './aggregationRequest';
-import { AggregationResponseNode } from './aggregationResponseNode';
 import { AggregationType } from './aggregationType';
 import { Allocation } from './allocation';
 import { AllocationRequest } from './allocationRequest';
@@ -560,7 +557,6 @@ import { MarketOptions } from './marketOptions';
 import { MetricValue } from './metricValue';
 import { ModelSelection } from './modelSelection';
 import { MovementType } from './movementType';
-import { NestedAggregationResponse } from './nestedAggregationResponse';
 import { OperandType } from './operandType';
 import { Operator } from './operator';
 import { OptionType } from './optionType';
@@ -902,7 +898,6 @@ let typeMap: {[index: string]: any} = {
     "AggregationOptions": AggregationOptions,
     "AggregationQuery": AggregationQuery,
     "AggregationRequest": AggregationRequest,
-    "AggregationResponseNode": AggregationResponseNode,
     "AggregationType": AggregationType,
     "Allocation": Allocation,
     "AllocationRequest": AllocationRequest,
@@ -1064,7 +1059,6 @@ let typeMap: {[index: string]: any} = {
     "MetricValue": MetricValue,
     "ModelSelection": ModelSelection,
     "MovementType": MovementType,
-    "NestedAggregationResponse": NestedAggregationResponse,
     "OperandType": OperandType,
     "Operator": Operator,
     "OptionType": OptionType,
