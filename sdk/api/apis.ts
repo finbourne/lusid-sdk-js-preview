7ba740597f82dbae254cfad92e963f1e1994c4b0
Concourse <concourse@finbourne.com>
2021-06-07 10:42:17 +0000

    Update SDK 0.11.3111

--- sdk/api/apis.ts
+++ sdk/api/apis.ts
@@ -68,7 +68,4 @@ export * from './transactionPortfoliosApi';
 import { TransactionPortfoliosApi } from './transactionPortfoliosApi';
 export * from './translationApi';
 import { TranslationApi } from './translationApi';
-
-export { RequestFile } from '../model/models';
-
 export const APIS = [AggregationApi, AllocationsApi, ApplicationMetadataApi, CalendarsApi, ConfigurationRecipeApi, ConventionsApi, CorporateActionSourcesApi, CounterpartyApi, CutLabelDefinitionsApi, DataTypesApi, DerivedTransactionPortfoliosApi, EntitiesApi, InstrumentsApi, LegalEntitiesApi, LoginApi, OrdersApi, PersonsApi, PortfolioGroupsApi, PortfoliosApi, PropertyDefinitionsApi, QuotesApi, ReconciliationsApi, ReferencePortfolioApi, RelationDefinitionsApi, RelationsApi, RelationshipDefinitionsApi, RelationshipsApi, SchemasApi, ScopesApi, SearchApi, StructuredMarketDataApi, StructuredResultDataApi, SystemConfigurationApi, TransactionPortfoliosApi, TranslationApi];
