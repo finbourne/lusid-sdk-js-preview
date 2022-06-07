9f9f3a09f1a7d0b571baa3c9898340601377e559
Concourse <concourse@finbourne.com>
2022-06-07 13:51:41 +0000

    Update SDK 0.11.4446 [publish]

--- sdk/model/models.ts
+++ sdk/model/models.ts
@@ -6,7 +6,6 @@ export * from './a2BDataRecord';
 export * from './a2BMovementRecord';
 export * from './accessControlledAction';
 export * from './accessControlledResource';
-export * from './accessMetadataOperation';
 export * from './accessMetadataValue';
 export * from './accountingMethod';
 export * from './actionId';
@@ -293,7 +292,6 @@ export * from './opaqueModelOptions';
 export * from './opaqueModelOptionsAllOf';
 export * from './operandType';
 export * from './operation';
-export * from './operationType';
 export * from './operator';
 export * from './order';
 export * from './orderBySpec';
@@ -637,7 +635,6 @@ import { A2BDataRecord } from './a2BDataRecord';
 import { A2BMovementRecord } from './a2BMovementRecord';
 import { AccessControlledAction } from './accessControlledAction';
 import { AccessControlledResource } from './accessControlledResource';
-import { AccessMetadataOperation } from './accessMetadataOperation';
 import { AccessMetadataValue } from './accessMetadataValue';
 import { AccountingMethod } from './accountingMethod';
 import { ActionId } from './actionId';
@@ -924,7 +921,6 @@ import { OpaqueModelOptions } from './opaqueModelOptions';
 import { OpaqueModelOptionsAllOf } from './opaqueModelOptionsAllOf';
 import { OperandType } from './operandType';
 import { Operation } from './operation';
-import { OperationType } from './operationType';
 import { Operator } from './operator';
 import { Order } from './order';
 import { OrderBySpec } from './orderBySpec';
@@ -1262,7 +1258,6 @@ let primitives = [
                  ];
 
 let enumsMap: {[index: string]: any} = {
-        "AccessMetadataOperation.OpEnum": AccessMetadataOperation.OpEnum,
         "AccountingMethod": AccountingMethod,
         "AggregateSpec.OpEnum": AggregateSpec.OpEnum,
         "AggregationOp": AggregationOp,
@@ -1404,7 +1399,6 @@ let enumsMap: {[index: string]: any} = {
         "OpaqueModelOptions.ModelOptionsTypeEnum": OpaqueModelOptions.ModelOptionsTypeEnum,
         "OpaqueModelOptionsAllOf.ModelOptionsTypeEnum": OpaqueModelOptionsAllOf.ModelOptionsTypeEnum,
         "OperandType": OperandType,
-        "OperationType": OperationType,
         "Operator": Operator,
         "OrderBySpec.SortOrderEnum": OrderBySpec.SortOrderEnum,
         "OutputTransaction.TransactionStatusEnum": OutputTransaction.TransactionStatusEnum,
@@ -1516,7 +1510,6 @@ let typeMap: {[index: string]: any} = {
     "A2BMovementRecord": A2BMovementRecord,
     "AccessControlledAction": AccessControlledAction,
     "AccessControlledResource": AccessControlledResource,
-    "AccessMetadataOperation": AccessMetadataOperation,
     "AccessMetadataValue": AccessMetadataValue,
     "ActionId": ActionId,
     "ActionResultOfPortfolio": ActionResultOfPortfolio,
