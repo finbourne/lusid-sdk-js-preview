e33eacd5d05d4e0a780f7d9009b6a23b9ee41b14
Concourse <concourse@finbourne.com>
2023-08-10 08:28:02 +0000

    Update SDK 1.0.429 [rebase]

--- sdk/model/getVirtualDocumentResponse.ts
+++ sdk/model/getVirtualDocumentResponse.ts
@@ -28,9 +28,6 @@ export class GetVirtualDocumentResponse {
     * The set of calues that could not be retrieved along with a reason for this, e.g. badly formed request.
     */
     'failed'?: { [key: string]: ErrorDetail; } | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
 
     static discriminator: string | undefined = undefined;
