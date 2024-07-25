93582c76722cff02d517465df7e3659628e04f1e
Matt Storey <matthew.storey@finbourne.com>
2024-07-25 09:32:13 +0100

    CTOOLS-278: Converted then to async

--- sdk/test/tutorials/instrumentMaster.test.ts
+++ sdk/test/tutorials/instrumentMaster.test.ts
@@ -10,235 +10,201 @@ import {
   InstrumentIdValue,
   Property, PropertyValue
 } from "../../api";
-import { client } from './clientBuilder'
+import { client } from './clientBuilder';
 
 enum FileType {
   Json = "Json",
   Csv = "Csv",
 }
 
-const uuid4 = require('uuid/v4')
-const csv = require('csvtojson')
-let uniqueScope = 'Performance' + uuid4()
-const debugHeaders = ['lusid-meta-requestid']
+const uuid4 = require('uuid/v4');
+const csv = require('csvtojson');
+let uniqueScope = 'Performance' + uuid4();
+const debugHeaders = ['lusid-meta-requestid'];
 
 // Create a custom property using the LUSID model
-let sectorDataTypeId = new ResourceId()
-sectorDataTypeId.scope = "system"
-sectorDataTypeId.code = "string"
+let sectorDataTypeId = new ResourceId();
+sectorDataTypeId.scope = "system";
+sectorDataTypeId.code = "string";
 
 // Create a property definition request to define a new property in LUSID
-let sectorProp = new CreatePropertyDefinitionRequest()
-sectorProp.domain = CreatePropertyDefinitionRequest.DomainEnum.Instrument
-sectorProp.scope = uniqueScope
-sectorProp.code = 'Sector'
-sectorProp.valueRequired = true
-sectorProp.displayName = 'Sector'
-sectorProp.dataTypeId = sectorDataTypeId
-sectorProp.lifeTime = CreatePropertyDefinitionRequest.LifeTimeEnum.TimeVariant
+let sectorProp = new CreatePropertyDefinitionRequest();
+sectorProp.domain = CreatePropertyDefinitionRequest.DomainEnum.Instrument;
+sectorProp.scope = uniqueScope;
+sectorProp.code = 'Sector';
+sectorProp.valueRequired = true;
+sectorProp.displayName = 'Sector';
+sectorProp.dataTypeId = sectorDataTypeId;
+sectorProp.lifeTime = CreatePropertyDefinitionRequest.LifeTimeEnum.TimeVariant;
 
 describe('Load Instrument Master', () => {
-  it('Should upsert instruments into LUSID', (done) => {
-    getInstrumentsFromFile('./instruments.json', FileType.Json)
-      .then((instruments: InstrumentDefinition[]) => {
-        upsertInstruments(instruments)
-          .then(() => done())
-          .catch((err) => done(`Failed to upsert instruments. Error: ${err.toString()}`))
-      })
-      .catch((err) => done(`Failed to get instruments to upsert. Error: ${err.toString()}`))
-  })
-})
+  it('Should upsert instruments into LUSID', async () => {
+    try {
+      const instruments = await getInstrumentsFromFile('./instruments.json', FileType.Json);
+      await upsertInstruments(instruments);
+    } catch (err) {
+      throw new Error(`Failed to upsert instruments. Error: ${err.toString()}`);
+    }
+  });
+});
 
 describe('Create property definitions', () => {
-  it('Should create a sector property in LUSID', (done) => {
-    createProperty(sectorProp)
-      .then(() => done())
-      .catch((err) => done(`Failed to create the sector property. Error: ${err.toString()}`))
-  })
-})
+  it('Should create a sector property in LUSID', async () => {
+    try {
+      await createProperty(sectorProp);
+    } catch (err) {
+      throw new Error(`Failed to create the sector property. Error: ${err.toString()}`);
+    }
+  });
+});
 
 describe('Add properties on instruments', () => {
-  it('Should add the sector property on each instrument', (done) => {
-    getPropertyRequestsFromFile('./instrument-properties.json', 'Sector', FileType.Json)
-      .then((requests: UpsertInstrumentPropertyRequest[]) => {
-        upsertInstrumentProperties(requests)
-          .then(() => done())
-          .catch((err) => done(`Failed to add sector property to instruments. Error: ${err.toString()}`))
-      })
-      .catch((err) => done(`Failed to get instruments to add properties to. Error: ${err.toString()}`))
-  })
-})
+  it('Should add the sector property on each instrument', async () => {
+    try {
+      const requests = await getPropertyRequestsFromFile('./instrument-properties.json', 'Sector', FileType.Json);
+      await upsertInstrumentProperties(requests);
+    } catch (err) {
+      throw new Error(`Failed to add sector property to instruments. Error: ${err.toString()}`);
+    }
+  });
+});
 
 // Import your instruments from a CSV file
-function loadFromCsv(filePath: string): Promise<any[]> {
-  return new Promise((resolve, reject) => {
-    // Use the csvtojson module to import a CSV file
-    csv()
-      .fromFile(filePath)
-      // Produces an array of objects, one for each row (instrument) of the CSV
-      .then((instruments: any[]) => resolve(instruments))
-      .catch((err) => reject(err))
-  })
+async function loadFromCsv(filePath: string): Promise<any[]> {
+  try {
+    const instruments = await csv().fromFile(filePath);
+    return instruments;
+  } catch (err) {
+    throw new Error(`Failed to load instruments from CSV. Error: ${err.toString()}`);
+  }
 }
 
 // Import your instruments from a JSON file
-function loadFromJson(filePath: string): Promise<any[]> {
-  return new Promise((resolve, reject) => {
-    try {
-      let instruments = require(filePath)
-      resolve(instruments)
-    } catch (err) {
-      reject(err)
-    }
-  })
+async function loadFromJson(filePath: string): Promise<any[]> {
+  try {
+    const instruments = require(filePath);
+    return instruments;
+  } catch (err) {
+    throw new Error(`Failed to load instruments from JSON. Error: ${err.toString()}`);
+  }
 }
 
 // Upsert a set of instrument definitions into LUSID
-function upsertInstruments(instruments: InstrumentDefinition[])
-  : Promise<UpsertInstrumentsResponse> {
-  return new Promise((resolve, reject) => {
-    let body: { [instrumentName: string]: InstrumentDefinition } = {}
+async function upsertInstruments(instruments: InstrumentDefinition[]): Promise<UpsertInstrumentsResponse> {
+  const body: { [instrumentName: string]: InstrumentDefinition } = {};
 
-    // Add each instrument definition to a dictionary
-    instruments.forEach(instrument => {
-      body[instrument.name] = instrument
-    });
+  instruments.forEach(instrument => {
+    body[instrument.name] = instrument;
+  });
 
-    client.api.instruments.upsertInstruments(body, uniqueScope)
-      .then((res: any) => {
-        getLoggingInfo(res?.response?.rawHeaders,debugHeaders,'upsertInstruments')
-        resolve(res.body)
-      })
-      .catch((err: any) => reject(err))
-  })
+  try {
+    const res = await client.api.instruments.upsertInstruments(body, uniqueScope);
+    getLoggingInfo(res?.response?.rawHeaders, debugHeaders, 'upsertInstruments');
+    return res.body;
+  } catch (err) {
+    throw new Error(`Failed to upsert instruments. Error: ${err.toString()}`);
+  }
 }
 
 // Use your client to call LUSID and create a new property
-function createProperty(
-  propertyDefintion: CreatePropertyDefinitionRequest):
-  Promise<PropertyDefinition> {
-  return new Promise((resolve, reject) => {
-    // Use your client to call create property definition
-    client.api.propertyDefinitions.createPropertyDefinition(propertyDefintion)
-      .then((res: any) => {
-        getLoggingInfo(res?.response?.rawHeaders,debugHeaders,'createPropertyDefinition')
-        resolve(res.body)
-      })
-      .catch((err: any) => reject(err))
-  })
+async function createProperty(propertyDefintion: CreatePropertyDefinitionRequest): Promise<PropertyDefinition> {
+  try {
+    const res = await client.api.propertyDefinitions.createPropertyDefinition(propertyDefintion);
+    getLoggingInfo(res?.response?.rawHeaders, debugHeaders, 'createPropertyDefinition');
+    return res.body;
+  } catch (err) {
+    throw new Error(`Failed to create property. Error: ${err.toString()}`);
+  }
 }
 
 // Construct a property that we can add to a LUSID instrument
 function buildInstrumentProperty(key: string, value: string): Property {
-  // Initialize the property key (Instrument/Scope/PropertyName)
-  let instrumentProperty = new Property()
-  instrumentProperty.key = key
+  let instrumentProperty = new Property();
+  instrumentProperty.key = key;
 
-  // Initialize the property value
-  let instrumentPropertyValue = new PropertyValue()
-  instrumentPropertyValue.labelValue = value
-  instrumentProperty.value = instrumentPropertyValue
+  let instrumentPropertyValue = new PropertyValue();
+  instrumentPropertyValue.labelValue = value;
+  instrumentProperty.value = instrumentPropertyValue;
 
-  return instrumentProperty
+  return instrumentProperty;
 }
 
 // Use your client to add a property to a particular instrument in LUSID
-function upsertInstrumentProperties(
-  requests: UpsertInstrumentPropertyRequest[]):
-  Promise<UpsertInstrumentPropertiesResponse> {
-  return new Promise((resolve, reject) => {
-    client.api.instruments.upsertInstrumentsProperties(requests, uniqueScope)
-      .then((res: any) => {
-        getLoggingInfo(res?.response?.rawHeaders,debugHeaders,'upsertInstrumentProperties')
-        resolve(res.body)
-      })
-      .catch((err: any) => reject(err))
-  })
+async function upsertInstrumentProperties(requests: UpsertInstrumentPropertyRequest[]): Promise<UpsertInstrumentPropertiesResponse> {
+  try {
+    const res = await client.api.instruments.upsertInstrumentsProperties(requests, uniqueScope);
+    getLoggingInfo(res?.response?.rawHeaders, debugHeaders, 'upsertInstrumentProperties');
+    return res.body;
+  } catch (err) {
+    throw new Error(`Failed to upsert instrument properties. Error: ${err.toString()}`);
+  }
 }
 
 // Load our instrument definitions from a file
-function getInstrumentsFromFile(
-  filePath: string,
-  fileType: FileType):
-  Promise<InstrumentDefinition[]> {
-  // Read our set of instruments objects from a file
+async function getInstrumentsFromFile(filePath: string, fileType: FileType): Promise<InstrumentDefinition[]> {
+  let loadFunction;
   if (fileType == FileType.Json) {
-    var loadFunction = loadFromJson(filePath)
+    loadFunction = loadFromJson(filePath);
   } else {
-    var loadFunction = loadFromCsv(filePath)
+    loadFunction = loadFromCsv(filePath);
   }
 
-  return loadFunction.then((instruments: any[]) => {
-    // Use a map function to convert each instrument object into a LUSID model
-    return instruments.map((instrument: any) => {
-      let definition: InstrumentDefinition = new InstrumentDefinition()
+  const instruments = await loadFunction;
+  return instruments.map((instrument: any) => {
+    let definition = new InstrumentDefinition();
 
-      let cusipIdentifier = new InstrumentIdValue()
-      cusipIdentifier.value = instrument.CUSIP
+    let cusipIdentifier = new InstrumentIdValue();
+    cusipIdentifier.value = instrument.CUSIP;
 
-      let isinIdentifier = new InstrumentIdValue()
-      isinIdentifier.value = instrument.ISIN
+    let isinIdentifier = new InstrumentIdValue();
+    isinIdentifier.value = instrument.ISIN;
 
-      let tickerIdentifier = new InstrumentIdValue()
-      tickerIdentifier.value = instrument.Ticker
+    let tickerIdentifier = new InstrumentIdValue();
+    tickerIdentifier.value = instrument.Ticker;
 
-      let figiIdentifier = new InstrumentIdValue()
-      figiIdentifier.value = instrument.Figi
+    let figiIdentifier = new InstrumentIdValue();
+    figiIdentifier.value = instrument.Figi;
 
-      definition.name = instrument.Name
-      definition.identifiers = {
-        "Cusip": cusipIdentifier,
-        "Isin": isinIdentifier,
-        "Ticker": tickerIdentifier,
-        "Figi": figiIdentifier
-      }
+    definition.name = instrument.Name;
+    definition.identifiers = {
+      "Cusip": cusipIdentifier,
+      "Isin": isinIdentifier,
+      "Ticker": tickerIdentifier,
+      "Figi": figiIdentifier
+    };
 
-      return definition
-    })
-  })
+    return definition;
+  });
 }
 
-// Construct a set of requests that will allow us to add new properties to LUSID instruments
-function getPropertyRequestsFromFile(
-  filePath: string,
-  propertyName: string,
-  fileType: FileType):
-  Promise<UpsertInstrumentPropertyRequest[]> {
-  // Load our property values from file
-  if (fileType == FileType.Json) {
-    var loadFunction = loadFromJson(filePath)
-  } else {
-    var loadFunction = loadFromCsv(filePath)
-  }
-
-  // Create an upsert request for each property in the file
-  return loadFunction.then((properties) => {
-    return properties.map((property) => {
-      let instrumentPropertyRequest = new UpsertInstrumentPropertyRequest()
-      instrumentPropertyRequest.identifierType = "Figi"
-      instrumentPropertyRequest.identifier = property.Figi
-      instrumentPropertyRequest.properties = [
-        buildInstrumentProperty(
-          `Instrument/${uniqueScope}/${propertyName}`,
-          property[propertyName]
-        )
-      ]
-      return instrumentPropertyRequest
-    })
-  })
+async function getPropertyRequestsFromFile(filePath: string, propertyName: string, fileType: FileType): Promise<UpsertInstrumentPropertyRequest[]> {
+  const loadFunction = fileType === FileType.Json ? loadFromJson(filePath) : loadFromCsv(filePath);
+  
+  const properties = await loadFunction;
+  return properties.map((property: any) => {
+    let instrumentPropertyRequest = new UpsertInstrumentPropertyRequest();
+    instrumentPropertyRequest.identifierType = "Figi";
+    instrumentPropertyRequest.identifier = property.Figi;
+    instrumentPropertyRequest.properties = [
+      buildInstrumentProperty(
+        `Instrument/${uniqueScope}/${propertyName}`,
+        property[propertyName]
+      )
+    ];
+    return instrumentPropertyRequest;
+  });
 }
 
-function getLoggingInfo(headers: string[], keysToSearch: string[], functionCallName: string)
-{
-  if(headers) {
-    for(const key of keysToSearch) {
-      const index = headers.findIndex((header)=> header===key)
-      if(index > -1){
-        console.log(`${functionCallName}: ${headers[index]}: ${headers[index+1]}`)
+
+function getLoggingInfo(headers: string[], keysToSearch: string[], functionCallName: string) {
+  if (headers) {
+    for (const key of keysToSearch) {
+      const index = headers.findIndex((header) => header === key);
+      if (index > -1) {
+        console.log(`${functionCallName}: ${headers[index]}: ${headers[index + 1]}`);
       }
     }
   }
-
 }
 
-export { };
+export {};
